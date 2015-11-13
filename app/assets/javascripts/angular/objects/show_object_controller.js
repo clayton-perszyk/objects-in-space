e0c1e46d4da11ab88e863a6e22ef6304246b1335
clayton-perszyk <clayperszyk@gmail.com>
2015-11-13 10:58:07 -0800

    neo controller bug fixes

--- app/assets/javascripts/angular/objects/show_object_controller.js
+++ app/assets/javascripts/angular/objects/show_object_controller.js
@@ -5,11 +5,4 @@ app.controller('ShowObjectController', ["$scope", "$http", "$routeParams", funct
     console.log(data);
     $scope.asteroid = data.data;
   });
-  // $scope.asteroid = {
-  //   name: '2010 PK9',
-  //   size: 0.1160259082,
-  //   close_approach_date: "1990/6/1",
-  //   close_approach_distance: 6659408.5,
-  //   velocity: 30.942357826
-  // };
 }]);
