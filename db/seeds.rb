e3b47786d659139f64776fab17eb35c29eafe72c
clayton-perszyk <clayperszyk@gmail.com>
2015-11-12 16:54:16 -0800

    add requests for obejects show and index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Nearearthobject.create([{
+    id: 1,
     name: '2010 PK9',
     size: 0.1160259082,
     close_approach_date: DateTime.new(1990, 06, 01),
     close_approach_distance: 6659408.5,
     velocity: 30.942357826,
   },
+  {
+    id: 2,
     name: '1999 VF22',
     size: 0.2111324448,
     close_approach_date: DateTime.new(2016, 02, 18),
     close_approach_distance: 14097968,
     velocity: 28.0780565472,
-  ]);
+  }
+]);
