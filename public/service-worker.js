b45a7be2f599e537555727dd4a29e00eab9d926c
clayton-perszyk <clayperszyk@gmail.com>
2015-11-17 14:39:04 -0800

    add notification formating

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,14 +2,21 @@
   'use strict';
 
   self.addEventListener('push', function(event) {
-
-    fetch('http://localhost:3000/nearearthobjects/1.json').then(function(response) {
+    fetch('https://objects-in-space.herokuapp.com/todays-neos.json').then(function(response) {
       response.json().then(function(data) {
-        var title = data.name;
-        var body = 'Its about to hit';
-        var icon = 'https://tse1.mm.bing.net/th?id=A12a0afe14901d061809d3f13ab8997d7&w=182&h=183&c=7&rs=1&qlt=90&pid=3.1&rm=2';
+        var title = '';
+        var body = 'Are about to hit';
+        var icon = 'https://objects-in-space.herokuapp.com/icon-192x192.png';
         var tag = 'simple-push-demo-notification-tag';
-
+        var names = [];
+        data.forEach(function(item) {
+          names.push(item.name);
+        });
+        if(names.length >= 3) {
+          title = "There are " + names.length + " NEOs about to hit";
+        } else {
+          title = names.join(", ");
+        }
         event.waitUntil(
           self.registration.showNotification(title, {
             body: body,
@@ -36,11 +43,11 @@
     }).then(function(clientList) {
       for (var i = 0; i < clientList.length; i++) {
         var client = clientList[i];
-        if (client.url == '/' && 'focus' in client)
+        if (client.url == '/#/NEO?q=filter' && 'focus' in client)
           return client.focus();
       }
       if (clients.openWindow)
-        return clients.openWindow('/');
+        return clients.openWindow('/#/NEO?q=filter');
     }));
 
   });
