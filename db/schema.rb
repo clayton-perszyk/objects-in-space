f41d5f05bc2b5a4d2ee6cc7a2a2dcb9b811b18a1
LionelLints <lionel.lints@gmail.com>
2015-11-17 09:56:30 -0800

    begin styling site

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20151113200831) do
+ActiveRecord::Schema.define(version: 20151116225553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
