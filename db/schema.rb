c5589aa7b7f945b7f95293b2d1574623dd1b6085
LionelLints <lionel.lints@gmail.com>
2015-11-18 13:05:16 -0800

    update styles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117194808) do
+ActiveRecord::Schema.define(version: 20151118193902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20151117194808) do
     t.string  "sizeCatagory"
     t.boolean "pha"
     t.string  "distanceCatagory"
+    t.string  "nickname"
   end
 
   create_table "subscribed_users", force: :cascade do |t|
--- lib/tasks/fake_names.rake
+++ lib/tasks/fake_names.rake
@@ -4,7 +4,7 @@ desc "add fake names to NEOs"
 
 task :fake_names => :environment do
   Nearearthobject.all.each_with_index do |o, i|
-    o.update(name: "#{Faker::Book.title} - #{i}")
+    o.update(nickname: "#{Faker::Book.title} - #{i}")
     o.save
   end
 end
